934887fe29742884482ce9665620c0078b95f4ba
parafara <leebadagyumin0129@gmail.com>
2025-11-18 18:02:31 +0900

    test: 서버 테스트 코드 제거

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from router import root
 from database import engine, Base
 import models
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-app.include_router(root.router)
--- service/root.py
+++
@@ -1,2 +0,0 @@
-def read_root_message():
-    return {"message": "hello world"}
