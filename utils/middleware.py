f3b146eb84679390355d2ee06cad8020d69b5611
cpdmpara <leebadagyumin0129@gmail.com>
2025-12-06 23:07:53 +0900

    fix: ID 비유효와 일반 not found를 구별하도록 응답 로직 수정

--- utils/middleware.py
+++ utils/middleware.py
@@ -2,7 +2,7 @@ from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse
 from utils.crypto_manager import CryptoManagerException
 from utils.request_manager import RequestManagerException
-from utils.constant import INVALID_TOKEN, EXPIRED_TOKEN, NOT_LOGGED_IN
+from utils.constant import INVALID_TOKEN, EXPIRED_TOKEN, NOT_LOGGED_IN, INVALID_ID
 
 async def exception_catcher(request: Request, next_call):
     try:
@@ -13,10 +13,10 @@ async def exception_catcher(request: Request, next_call):
             raise HTTPException(status_code=401, detail=INVALID_TOKEN)
         except CryptoManagerException.ExpiredToken:
             raise HTTPException(status_code=401, detail=EXPIRED_TOKEN)
-        except CryptoManagerException.InvalidId:
-            raise HTTPException(status_code=404)
         except RequestManagerException.NotLoggedIn:
             raise HTTPException(status_code=401, detail=NOT_LOGGED_IN)
+        except CryptoManagerException.InvalidId:
+            raise HTTPException(status_code=404, detail=INVALID_ID)
     except HTTPException as e:
         response = JSONResponse(
             content = {"detail": e.detail},
