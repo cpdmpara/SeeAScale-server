f16ac6b93187fd756f945d8a9dae7ce7a79622a1
cpdmpara <leebadagyumin0129@gmail.com>
2025-12-20 15:13:32 +0900

    fix: 회원가입용 이메일 내용을 프론트엔드 링크로 수정

--- utils/mail_manager.py
+++ utils/mail_manager.py
@@ -1,13 +1,16 @@
 import smtplib
 from email.message import EmailMessage
-from utils.constant import SMTP_SERVER_ADDRESS, SMTP_PORT, SMTP_MAIL_ADDRESS, SMTP_PASSWORD
+from utils.constant import SMTP_SERVER_ADDRESS, SMTP_PORT, SMTP_MAIL_ADDRESS, SMTP_PASSWORD, FRONTEND_HOST
 
-def send_preregister_mail(To: str, Message: str):
+def send_preregister_mail(To: str, signUpToken: str):
     msg = EmailMessage()
-    msg.set_content(Message)
     msg['To'] = To
     msg['From'] = SMTP_MAIL_ADDRESS
     msg['Subject'] = "[See A Scale] 회원가입 이메일 인증"
+    url = f"{FRONTEND_HOST}/signup?pretoken={signUpToken}"
+    msg.set_content(url)
+    html_content = f"""<html><body><p>See A Scale의 회원가입을 원하신다면 아래 '회원가입'을 눌러주세요!</p><a href="{url}">회원가입</a></body></html>"""
+    msg.add_alternative(html_content, subtype="html")
 
     with smtplib.SMTP_SSL(SMTP_SERVER_ADDRESS, SMTP_PORT) as server:
         server.login(SMTP_MAIL_ADDRESS, SMTP_PASSWORD)
